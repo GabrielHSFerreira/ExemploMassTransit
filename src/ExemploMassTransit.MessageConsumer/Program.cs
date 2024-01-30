fa51d8676b8cef6437bc515d41523744a832fb44
Gabriel Henrique da Silva Ferreira <ghenrique_10@hotmail.com>
2024-01-30 09:54:40 -0300

    Initial.

---
+++ src/ExemploMassTransit.MessageConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploMassTransit.MessageConsumer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
